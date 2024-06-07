bf255eafa285ac5c97b703391980ea3411934fb3
Daniil_Bukov <bukovda@gmail.com>
2024-06-07 20:03:18 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+MyClass myClass = new MyClass(1, "2");
+
+int size = int.Parse(Console.ReadLine());
+
+MyClass[] myClasses = new MyClass[size];
+
+Random rand = new Random();
+
+
+
+for (int i = 0; i < size; i++)
+{
+    myClasses[i] = new MyClass(rand.Next(-256, 256), GenerateRandom(16));
+
+}
+
+foreach (MyClass _class in myClasses)
+{
+    Console.WriteLine($"MyInt: {_class.MyInt}");
+    Console.WriteLine($"MyString:{_class.MyString}\n");
+}
+
+
+string GenerateRandom(int _maxSize)
+{
+    string str = "";
+    for (int i = 0; i <= rand.NextInt64(0, _maxSize); i++)
+    {
+        str += Convert.ToChar(rand.Next(65, 122));
+    }
+    return str;
+}
+
+class MyClass
+{
+    public int MyInt { get; init; }
+
+    public string MyString { get; init; }
+
+    public MyClass(int myInt, string myString)
+    {
+        MyInt = myInt;
+        MyString = myString;
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+
+Type myType = typeof(MyClass);
+
+foreach (var method in myType.GetMethods())
+{
+    Console.WriteLine(method.Name);
+}
+
+class MyClass
+{
+    private static void PrintString(string str) => Console.WriteLine(str);
+
+}
