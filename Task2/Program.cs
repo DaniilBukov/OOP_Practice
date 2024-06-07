8fb796d9e4f50522c4ff382faf87f3fca7974e6b
Daniil_Bukov <bukovda@gmail.com>
2024-06-07 20:52:21 +0300

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,17 +1,22 @@
 ﻿
+using System.Reflection;
 
-
-
-
+// Получаем тип
 Type myType = typeof(MyClass);
 
-foreach (var method in myType.GetMethods())
-{
-    Console.WriteLine(method.Name);
-}
+// Получаем метод - обязательно ставим BindingFlags
+var myMethod = myType.GetMethod("PrintString", BindingFlags.Static | BindingFlags.NonPublic);
 
+//Поверяем, есть ли такой метод
+if (myMethod != null)
+{
+    // Выводим название метода
+    Console.WriteLine($"{myMethod.Name}");
+
+    // Вызываем метод, 1 параметр - ссылка на объект (его нет, так как метод статический), второй параметр - массив объектов-аргументов(все классы наследуются от object)
+    myMethod.Invoke(null, new object[] { "123" });
+}
 class MyClass
 {
     private static void PrintString(string str) => Console.WriteLine(str);
-
 }
